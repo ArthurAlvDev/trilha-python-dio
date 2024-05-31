e7ba57a9f53d3ac126345470b82efc5124a0bff1
ArthurAlvDev <69433870+ArthurAlvDev@users.noreply.github.com>
2024-05-31 16:26:51 -0300

    Update desafio.py

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -18,32 +18,24 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Valor depósito: "))
 
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("ERROR! Valor invalido!")
 
     elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
+        valor = float(input("Valor saque: "))
 
-        excedeu_saldo = valor > saldo
+        if valor > saldo:
+            print("ERROR! Saldo insuficiente.")
 
-        excedeu_limite = valor > limite
+        elif valor > limite:
+            print("ERROR! Limite insuficiente!")
 
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+        elif numero_saques >= LIMITE_SAQUES:
+            print("ERROR! Sem limite de saque!")
 
         elif valor > 0:
             saldo -= valor
@@ -51,16 +43,13 @@ while True:
             numero_saques += 1
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("ERROR! Valor invalido!")
 
     elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
 
     elif opcao == "q":
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("ERROR! Selecione Novamente!")
